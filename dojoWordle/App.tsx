8df76368ffb95a4a6c4b3aafbc886e8276cf3961
Nicolas Wagner <nicolasw@bam.tech>
2022-10-10 13:41:59 +0200

    add keyboard

--- dojoWordle/App.tsx
+++ dojoWordle/App.tsx
@@ -1,11 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Keyboard } from "./components/Keyboard";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View></View>
+      <Keyboard
+        onDelPress={() => {}}
+        onEnterPress={() => {}}
+        onKeyPress={() => {}}
+      />
     </View>
   );
 }
@@ -13,8 +19,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#FFFFFF",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
