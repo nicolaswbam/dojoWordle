f8ab047f32859830043fc2cdaa37c557a6ec9838
Hugo Chollet <hugochollet@Hugos-MBP.lan>
2022-11-03 15:56:33 +0100

    update app to styled component

--- dojoWordle/App.tsx
+++ dojoWordle/App.tsx
@@ -1,25 +1,24 @@
+import styled from "@emotion/native";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+
 import { Game } from "./components/Game";
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <StatusBar style="auto" />
-      <View style={styles.container}>
+      <GameContainer>
         <Game />
-      </View>
+      </GameContainer>
     </SafeAreaProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#FFFFFF",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+const GameContainer = styled.View({
+  flex: 1,
+  backgroundColor: "#FFFFFF",
+  alignItems: "center",
+  justifyContent: "center",
 });
